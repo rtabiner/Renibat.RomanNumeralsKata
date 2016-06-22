4d4d8c62b3206fb88c39185f59c9b0ccd03a7815
rtabiner <rtabiner@hotmail.co.uk>
2016-06-22 14:54:08 +0100

    Initial commit

---
+++ Renibat.RomanNumeralsKata/Services/RomanNumeralGenerator.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Renibat.RomanNumeralsKata.Services
+{
+    public class RomanNumeralGenerator
+    {
+        public string Generate(int number)
+        {
+            var result = "";
+
+            var numbers = new[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            var numerals = new[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (number >= numbers[i] && number > 0)
+                {
+                    result += numerals[i];
+
+                    number -= numbers[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Renibat.RomanNumeralsKata/Tests/WhenTestingTheRomanNumeralGenerator.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Renibat.RomanNumeralsKata.Services;
+
+namespace Renibat.RomanNumeralsKata.Tests
+{
+    [TestFixture]
+    public class WhenTestingTheRomanNumeralGenerator
+    {
+        protected RomanNumeralGenerator RomanNumeralGenerator { get; set; }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(3999, "MMMCMXCIX")]
+        public void ThenCorrectRomanNumeralsAreReturnedForNumber(int inputNumber, string expectedResult)
+        {
+            var romanNumeralGenerator = new RomanNumeralGenerator();
+
+            var result = romanNumeralGenerator.Generate(inputNumber);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }    
+}
